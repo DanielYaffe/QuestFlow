06e2413f6a41f827af2ba2415ad471a796fad270
NoobMajesty <47846708+NoobMajesty@users.noreply.github.com>
2026-08-02 21:30:22 +0300

    Feat: extended the size of max file size

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -41,8 +41,8 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
   customSiteTitle: 'QuestFlow API Documentation'
 }));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cors())
 
 app.use('/auth', authRouter);
